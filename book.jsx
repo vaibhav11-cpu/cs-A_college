3d6ca7a4ca114805aa6dce0172cf18c2c63e146d
VAIBHAV GOYAL <vaibhavghgoyal567@gmail.com>
2025-10-31 12:47:58 +0530

    app.jsx

---
+++ book.jsx
@@ -0,0 +1,14 @@
+const App = ()=>{
+    return (
+        <div>
+            <Book>
+                <img src="https://encrypted-tbh" alt="" />
+                const data=[
+                    {image: ""}
+                    {image: ""}
+                    {image: ""}
+                ]
+            </Book>
+        </div>
+    )
+}
